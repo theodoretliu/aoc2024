391fc7a696c18d7076433dd1e2d3e2526cb3b7e6
Theodore Liu <theodoretliu@gmail.com>
2024-12-10 23:23:42 -0800

    Improve the code

--- day11.rs
+++ day11.rs
@@ -5,10 +5,10 @@ fn main() {
     let stdin = io::stdin();
     let line = stdin.lock().lines().next().unwrap().unwrap();
 
-    let numbers: Vec<i64> = line
-        .split_whitespace()
-        .map(|s| s.parse().unwrap())
-        .collect();
+    let mut numbers = Vec::new();
+    for num_str in line.split_whitespace() {
+        numbers.push(num_str.parse::<i64>().unwrap());
+    }
 
     part1(&numbers);
     part2(&numbers);
@@ -16,84 +16,77 @@ fn main() {
 
 fn part1(numbers: &[i64]) {
     fn process(numbers: &[i64]) -> Vec<i64> {
-        let mut working = Vec::new();
+        let mut result = Vec::new();
 
-        for number in numbers {
-            let num_digits = number.to_string().len();
+        for &number in numbers {
+            let num_str = number.to_string();
+            let num_digits = num_str.len();
 
-            if *number == 0 {
-                working.push(vec![1]);
+            if number == 0 {
+                result.push(1);
             } else if num_digits % 2 == 0 {
-                working.push(vec![
-                    number.to_string()[..num_digits / 2].parse().unwrap(),
-                    number.to_string()[num_digits / 2..].parse().unwrap(),
-                ]);
+                let (first_half, second_half) = num_str.split_at(num_digits / 2);
+                result.push(first_half.parse().unwrap());
+                result.push(second_half.parse().unwrap());
             } else {
-                working.push(vec![number * 2024]);
+                result.push(number * 2024);
             }
         }
 
-        let mut result = Vec::new();
-
-        for vec in working {
-            result.extend(vec);
-        }
-
-        return result;
+        result
     }
 
-    let mut new = numbers.to_vec();
+    let mut current_numbers = numbers.to_vec();
+    const ITERATIONS: i32 = 25;
 
-    for _ in 0..25 {
-        new = process(&new);
+    for _ in 0..ITERATIONS {
+        current_numbers = process(&current_numbers);
     }
 
-    println!("{:?}", new.len());
+    println!("{}", current_numbers.len());
 }
 
 fn part2(numbers: &[i64]) {
-    let mut cache: HashMap<(i64, i64), i64> = HashMap::new();
-
     fn solve(num: i64, remaining_depth: i64, cache: &mut HashMap<(i64, i64), i64>) -> i64 {
+        // Base case
         if remaining_depth == 0 {
             return 1;
         }
 
-        if cache.contains_key(&(num, remaining_depth)) {
-            return cache[&(num, remaining_depth)];
+        // Check cache
+        if let Some(&result) = cache.get(&(num, remaining_depth)) {
+            return result;
         }
 
-        let result = {
-            if num == 0 {
-                solve(1, remaining_depth - 1, cache)
-            } else {
-                let length = num.to_string().len();
+        let result = if num == 0 {
+            solve(1, remaining_depth - 1, cache)
+        } else {
+            let num_str = num.to_string();
+            let num_digits = num_str.len();
 
-                if length % 2 == 0 {
-                    solve(
-                        num.to_string()[..length / 2].parse().unwrap(),
-                        remaining_depth - 1,
-                        cache,
-                    ) + solve(
-                        num.to_string()[length / 2..].parse().unwrap(),
-                        remaining_depth - 1,
-                        cache,
-                    )
-                } else {
-                    solve(num * 2024, remaining_depth - 1, cache)
-                }
+            if num_digits % 2 == 0 {
+                let (first_half, second_half) = num_str.split_at(num_digits / 2);
+                let first_num = first_half.parse().unwrap();
+                let second_num = second_half.parse().unwrap();
+
+                solve(first_num, remaining_depth - 1, cache)
+                    + solve(second_num, remaining_depth - 1, cache)
+            } else {
+                solve(num * 2024, remaining_depth - 1, cache)
             }
         };
 
         cache.insert((num, remaining_depth), result);
-
-        return result;
+        result
     }
 
-    let mut sum = 0;
-    for number in numbers {
-        sum += solve(*number, 75, &mut cache);
+    let mut total = 0;
+    const MAX_DEPTH: i64 = 75;
+    let mut cache = HashMap::new();
+
+    for &number in numbers {
+        total += solve(number, MAX_DEPTH, &mut cache);
     }
 
-    println!("{:?}", sum);
+    println!("{}", total);
 }
