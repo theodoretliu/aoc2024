254397098f00129b7d9970bd167780e861e0ed13
Theodore Liu <theodoretliu@gmail.com>
2024-12-13 17:42:31 -0800

    Refactor with some AI

--- day12.rs
+++ day12.rs
@@ -46,7 +46,6 @@ fn part1(grid: &[Vec<char>]) {
             }
         }
 
-        // Calculate area and fence based on number of neighbors
         let (additional_area, additional_fence) = match neighbors.len() {
             0 => (1, 4),
             1 => (1, 3),
@@ -57,10 +56,6 @@ fn part1(grid: &[Vec<char>]) {
         };
 
         (result.0 + additional_area, result.1 + additional_fence)
-    }
-
-    fn is_valid_position(x: i64, y: i64, grid: &[Vec<char>]) -> bool {
-        x >= 0 && y >= 0 && x < grid.len() as i64 && y < grid[0].len() as i64
     }
 
     let mut visited = HashSet::new();
@@ -100,25 +95,14 @@ fn part2(grid: &[Vec<char>]) {
             direction_pairs.push((DIRECTIONS[i], DIRECTIONS[(i + 1) % DIRECTIONS.len()]));
         }
 
-        for dir in DIRECTIONS {
-            let pls1 = (i as i64 + dir.0, j as i64 + dir.1);
-        }
-
         let mut tot_area = 1;
-
-        let mut tot_internal_angle = 0;
-
         let mut num_adjacent_neighbor_pairs = 0;
-
-        let mut num_concave_points = 0;
-
         let mut num_corners = 0;
 
         for (d1, d2) in direction_pairs {
             let pos1 = (i as i64 + d1.0, j as i64 + d1.1);
             let pos2 = (i as i64 + d2.0, j as i64 + d2.1);
 
-            // check that both neighbors are in the grid and have the same character
             if is_valid_position(pos1.0, pos1.1, grid)
                 && is_valid_position(pos2.0, pos2.1, grid)
                 && grid[pos1.0 as usize][pos1.1 as usize] == current_char
@@ -131,13 +115,8 @@ fn part2(grid: &[Vec<char>]) {
                 );
                 let corner_pos = (i as i64 + corner_dir.0, j as i64 + corner_dir.1);
 
-                // println!("{:?} {:?}", corner_dir, corner_pos);
-                assert!(is_valid_position(corner_pos.0, corner_pos.1, grid));
-
                 if grid[corner_pos.0 as usize][corner_pos.1 as usize] != current_char {
-                    // println!("{:?} {:?}", corner_pos, corner_dir);
                     num_corners += 1;
-                    num_concave_points += 1;
                 }
             }
         }
@@ -151,12 +130,10 @@ fn part2(grid: &[Vec<char>]) {
                 && grid[new_x as usize][new_y as usize] == current_char
             {
                 num_neighbors += 1;
-
                 let (inc_tot_area, inc_num_corners) =
                     visit(grid, (new_x as usize, new_y as usize), visited);
                 tot_area += inc_tot_area;
                 num_corners += inc_num_corners;
-                // tot_internal_angle += _tot_internal_angle;
             }
         }
 
@@ -166,18 +143,15 @@ fn part2(grid: &[Vec<char>]) {
             2 => {
                 if num_adjacent_neighbor_pairs == 1 {
                     1
-                } else if num_adjacent_neighbor_pairs == 0 {
-                    0
                 } else {
-                    unreachable!("Invalid number of neighbors");
+                    0
                 }
             }
-            3 => 0,
-            4 => 0,
+            3 | 4 => 0,
             _ => unreachable!("Invalid number of neighbors"),
         };
 
-        return (tot_area, num_corners);
+        (tot_area, num_corners)
     }
 
     let mut visited = HashSet::new();
